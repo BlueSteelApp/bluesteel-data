de63a9fb6d2920ddf5d538d42bdbf674738d7643
jkbluesteel <jkbluesteel>
2020-03-12 11:09:09 -0400

    Remove re-added files

--- app/GraphQL/query.js
+++
@@ -1,5 +0,0 @@
-import { gql } from 'apollo-server-express';
-import * as db from '../database';
-let o={name:'query',graphQLType:'PersonQuery',graphQLPlural:'PersonQueries'};
-export const typeDefs = gql(db.generateTypeDefs(o));
-export const resolvers = db.generateGraphQLImpl(o);
