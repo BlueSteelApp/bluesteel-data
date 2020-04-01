71a0c0342c18ca8bb72a88e3de0078ca70db195a
jkbluesteel <jkbluesteel>
2020-04-01 09:35:11 -0400

    Use worker-threads in the test

--- signups/test-signups.js
+++ signups/test-signups.js
@@ -1,25 +1,46 @@
-const [count=10000, concurrent=50] = process.argv.slice(2);
+
 
 const axios = require('axios');
 const qs=require('querystring');
 const async=require('async');
 
-async function run() {
+const {
+  Worker, isMainThread, parentPort, workerData
+} = require('worker_threads');
+
+async function runMainThread() {
+	const [count=1000, concurrent=50] = process.argv.slice(2);
+	const workers = [];
+	const results = [];
+	for(let i=0; i<concurrent; i++) {
+		workers.push(new Promise((resolve,reject) => {
+			const worker = new Worker(__filename,{workerData:{count: count,workerId:i}});
+			worker.on('message', r => results.push({workerId: i, results:r}));
+      worker.on('error', reject);
+      worker.on('exit', (code) => {
+        if (code !== 0)
+          reject(new Error(`Worker stopped with exit code ${code}`));
+      });
+		}));
+	}
+	await Promise.all(workers);
+}
+
+async function runChild() {
+	const {workerId:wid,count}=workerData;
 	const signups = [];
 	for(let i=0;i<count;i++) {
 		signups.push({
 			index:i,
-			given_name:'given_name_'+i,
-			family_name: 'family_name_'+i,
-			email: 'email_'+i,
-			phone: 'phone_'+i,
-			source_code: 'test_'+(i%100)
+			given_name:'given_name_'+wid+'_'+i,
+			family_name: 'family_name_'+wid+'_'+i,
+			email: 'email_'+wid+'_'+i,
+			phone: 'phone_'+wid+'_'+i,
+			source_code: 'test_'+wid
 		});
 	}
-
-	let errors = [];
-
-	const results = await async.mapLimit(signups, concurrent, (signup,cb) => {
+	const errors=[];
+	const results = await async.mapSeries(signups, (signup, cb) => {
 		let start = new Date();
 		const {index}=signup;
 		if(index%500==0)console.log('signup:',index);
@@ -34,8 +55,12 @@ async function run() {
 				cb(null, {index,result,time});
 			});
 	});
-	console.log(results);
-	console.log('errors:',errors.length);
+	parentPort.post({results,errors});
+}
+
+async function run() {
+	if(isMainThread) return runMainThread();
+	else return runChild();
 }
 
 run()
