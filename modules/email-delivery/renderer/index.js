7fffb67f5981f0533a4502721e0372cc498e855e
Chris Steele <chris@bluesteel.app>
2020-05-03 09:54:52 -0400

    Make the testing link parser asynchronous

--- modules/email-delivery/renderer/index.js
+++ modules/email-delivery/renderer/index.js
@@ -18,7 +18,6 @@ EmailDeliveryRenderer.prototype.initialize=function() {
 
 	//Turn links into handlebars fields
 	let {links,text_body,html_body}=this.convertLinksToHandlebars(this);
-	console.log("Text body=",text_body);
 	//This contains the actual links in the content
 	this.links=links;
 	this.linkTemplates=links.map(l=>{return Handlebars.compile(l.href);});
@@ -46,13 +45,13 @@ EmailDeliveryRenderer.prototype.getLinks=function(){
 	return this.links;
 }
 
-const urlMatcher=/((http(s)?(:\/\/))+(www\.)?([\w\-./])*(\.[a-zA-Z]{2,3}\/?))[^\s\b\n|]*[^.,;:?!@^$ -]/g;
+const textUrlMatcher=/((http(s)?(:\/\/))+(www\.)?([\w\-./])*(\.[a-zA-Z]{2,3}\/?))[^\s\b\n|]*[^.,;:?!@^$\s -]/g;
 
 EmailDeliveryRenderer.prototype.convertLinksToHandlebars=function(opts){
 	let {html_body,text_body}=opts ||{};
 	if (!text_body) throw new Error("extractLinks requires at least a text_body option");
 	let links=[];
-	let rewrote_text=(text_body || "").replace(urlMatcher,href=>{
+	let rewrote_text=(text_body || "").replace(textUrlMatcher,href=>{
 		links.push({location:"text_body",href:href});
 		return "{{{_links.["+(links.length-1)+"]}}}";
 	});
@@ -65,7 +64,7 @@ EmailDeliveryRenderer.prototype.convertLinksToHandlebars=function(opts){
 			links.push({location:"html_body",href:href.trim()});
 			$(value).attr('href',"{{{_links.["+(links.length-1)+"]}}}");
 		});
-		rewrote_html=$.html();
+		rewrote_html=$.html().trim();
 	}
 
 	let out={
@@ -79,21 +78,32 @@ EmailDeliveryRenderer.prototype.convertLinksToHandlebars=function(opts){
 
 EmailDeliveryRenderer.prototype.validate=function() {};
 
-EmailDeliveryRenderer.prototype.render=function({delivery,person}) {
+EmailDeliveryRenderer.prototype.render=async function(o) {
+	let {delivery,person}=o;
+	if (!delivery.id){
+		console.error("Invalid options",o);
+		throw "A delivery.id, the unique id of the delivery, is required";
+	}
 	let allMerges=Object.assign({},{source_code:this.source_code},person,delivery); //delivery info trumps person info
 	//First merge in the merge fields
 	allMerges._links=this.linkTemplates.map(f=>f(allMerges));
+
 	//Then if we have a link rewriter, do that.
-	if (this.linkRewriter) allMerges._links=allMerges._links.map((l,i)=>this.linkRewriter(l,i));
-
-	let text_body=this.textTemplate(allMerges);
-	let html_body=this.htmlTemplate?this.htmlTemplate(allMerges):null;
-
-	return {
-			subject:this.subjectTemplate(allMerges),
-			text_body,
-			html_body
+	const rewriteLinks = async () => {
+		return Promise.all(allMerges._links.map((l,i)=>this.linkRewriter(l,delivery.id,i)))
 	};
+
+	return rewriteLinks().then(links=>{
+		allMerges._links=links;
+
+		let text_body=this.textTemplate(allMerges);
+		let html_body=this.htmlTemplate?this.htmlTemplate(allMerges):null;
+		return {
+				subject:this.subjectTemplate(allMerges),
+				text_body,
+				html_body
+		};
+	});
 };
 
 module.exports = EmailDeliveryRenderer;
--- modules/email-delivery/renderer/render-test.js
+++ modules/email-delivery/renderer/render-test.js
@@ -1,5 +1,12 @@
 const assert=require('assert');
+const faker=require('faker');
 const EmailDeliveryRenderer = require('./');
+
+function delay(t, v) {
+   return new Promise(function(resolve) {
+       setTimeout(resolve.bind(null, v), t)
+   });
+}
 
 describe('email render wrapper', function() {
 	let email_blast={
@@ -15,24 +22,27 @@ here's a link: http://google.com?utm_source={{source_code}}
 
 and here's another link: http://google.com?utm_source={{source_code}}`,
 		html_body: `Hello {{given_name}},
-			<p>Here's a link <a href="http://google.com?utm_source={{source_code}}">to google</a></p>
-			<p>Here's another link <a href="http://google.com?utm_source={{source_code}}">to google</a></p>`,
+<p>Here's a link <a href="http://google.com?utm_source={{source_code}}">to google</a></p>
+<p>Here's another link <a href="http://google.com?utm_source={{source_code}}">to google</a></p>`,
 		status: 'LIST_BUILT',
-		linkRewriter:function(link,i){
-			return 'https://rewriter.com/?linkIndex='+i+'&url='+escape(link);
+		linkRewriter:function(link,delivery_id,i){
+			let out='https://rewriter.com/?delivery_id='+delivery_id+'&linkIndex='+i+'&url='+escape(link);
+			return delay(100).then(()=>{return out;});
 		}
 	};
 
-	let delivery={email_blast_id:1, person_id:1, person_email: 'larry@zoolander.com', status: 0};
+	let delivery={id:123,email_blast_id:1, person_id:1, person_email: 'larry@zoolander.com', status: 0};
 	let person={given_name:"Larry", family_name:"Zoolander",email:'larry@zoolander.com'};
 
 	const renderer = new EmailDeliveryRenderer(email_blast);
 	renderer.initialize();
 	it('should return the handlebars merge fields', function() {
-		assert.deepEqual(renderer.getReferencedFields(),["given_name","source_code"]);
+		let fields=renderer.getReferencedFields();
+		assert.deepEqual(fields,["given_name","source_code"]);
 	});
-	it('should merge person information into an email blast subject', function() {
-		let {subject}=renderer.render({delivery,person});
+	it('should merge person information into an email blast subject', async function() {
+		let out=await renderer.render({delivery,person});
+		let {subject}=out;
 		assert.deepEqual({subject:`Hello Larry`},{subject});
 	});
 	it('should get links', function() {
@@ -44,19 +54,59 @@ and here's another link: http://google.com?utm_source={{source_code}}`,
 				{location:"html_body",href:"http://google.com?utm_source={{source_code}}"}
 			],links);
 	});
-	it('should render the html', function() {
-		let {text_body,html_body}=renderer.render({delivery,person});
-		console.log("Text Body output=",text_body);
-		console.log("HTML Body output=",html_body);
+	it('should render the html',async function() {
+		let {text_body,html_body}=await renderer.render({delivery,person});
+		//console.log("Text Body output=",text_body);
+		//console.log("HTML Body output=",html_body);
 		//
 		assert.equal(text_body,`Hello Larry,
 
-here's a link: https://rewriter.com/?linkIndex=0&url=http%3A//google.com%3Futm_source%3DEM_123_ABC
+here's a link: https://rewriter.com/?delivery_id=123&linkIndex=0&url=http%3A//google.com%3Futm_source%3DEM_123_ABC
 
-and here's another link: https://rewriter.com/?linkIndex=1&url=http%3A//google.com%3Futm_source%3DEM_123_ABC`);
-		assert.equal(html_body,` <html><head></head><body>Hello Larry,
-	<p>Here&apos;s a link <a href="https://rewriter.com/?linkIndex=2&url=http%3A//google.com%3Futm_source%3DEM_123_ABC">to google</a></p>
-	<p>Here&apos;s another link <a href="https://rewriter.com/?linkIndex=3&url=http%3A//google.com%3Futm_source%3DEM_123_ABC">to google</a></p>
-</body></html>`);
+and here's another link: https://rewriter.com/?delivery_id=123&linkIndex=1&url=http%3A//google.com%3Futm_source%3DEM_123_ABC`);
+		assert.equal(html_body,`<html><head></head><body>Hello Larry,
+<p>Here&apos;s a link <a href="https://rewriter.com/?delivery_id=123&linkIndex=2&url=http%3A//google.com%3Futm_source%3DEM_123_ABC">to google</a></p>
+<p>Here&apos;s another link <a href="https://rewriter.com/?delivery_id=123&linkIndex=3&url=http%3A//google.com%3Futm_source%3DEM_123_ABC">to google</a></p></body></html>`);
+	});
+
+	it('should render 10K messages in <1 second',async function(){
+		console.log("Generating fake data ....");
+		let deliveries=Array.apply(null, Array(10000)).map((e,i)=>{
+			let email=faker.internet.email();
+			return {
+				delivery:{
+					id:i+1000000,
+					person_id:i,
+					person_email:email,
+					status:0
+				},
+				person:{
+					person_id:i,
+					given_name:faker.name.firstName(),
+					family_name:faker.name.lastName(),
+					email,
+					phone:faker.phone.phoneNumber(),
+					street_1:faker.address.streetAddress(),
+					street_2:faker.address.secondaryAddress(),
+					city:faker.address.city(),
+					region:faker.address.stateAbbr(),
+					postal_code:faker.address.zipCode()
+				}
+			}
+		});
+
+		console.log("Finished generating data, executing performance test");
+
+		let start=new Date().getTime();
+
+		let output=await Promise.all(deliveries.map(d=>{
+			return renderer.render(d)
+		}));
+		let end=new Date().getTime();
+		console.log("Sample random output",output[10]);
+		console.log("Sample random output",output[100]);
+		console.log("Sample random output",output[1000]);
+		console.log("Total time=",(end-start)," millis");
+		assert.ok((end-start)<1000);
 	});
 });
